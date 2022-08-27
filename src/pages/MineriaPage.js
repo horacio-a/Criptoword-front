3aa7a8ef87e7afbda9670979da3dbadde6a11d34
horacio albornoz <horaciomatiasalbornoz@gmail.com>
2022-08-27 18:08:12 -0300

    PRUEBA

--- src/pages/MineriaPage.js
+++ src/pages/MineriaPage.js
@@ -3,57 +3,57 @@ import AsicsItem from "../componets/mineria/asics";
 import axios from 'axios';
 
 
-const MineriaPage = (props) => {
-    const [loading, setLoading] = useState(false)
-    const [mineria, setMineria] = useState([])
-    useEffect(() => {
-        const cargarMineria = async () => {
-            setLoading(true);
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/mineria`);
-            setMineria(response.data)
-            setLoading(false)
-        }
-        cargarMineria();
+// const MineriaPage = (props) => {
+//     const [loading, setLoading] = useState(false)
+//     const [mineria, setMineria] = useState([])
+//     useEffect(() => {
+//         const cargarMineria = async () => {
+//             setLoading(true);
+//             const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/mineria`);
+//             setMineria(response.data)
+//             setLoading(false)
+//         }
+//         cargarMineria();
 
-    }, []);
-
-
-
-    return (
-        <div>
-            <div class='barraseparar'></div>
-            <div id='titulos'>
-                <h2>Mineria con GPU</h2>
-            </div>
-            <div class='barraseparar'></div>
-            <section className="holder"> </section>
-            <div class='barraseparar'></div>
-
-            <div className="center">
-                {
-                    loading ? (
-                        <p>cargando...</p>
-
-                    ) : (
-
-                        mineria.map(item => <AsicsItem key={item.id}
-                            name={item.nombre}
-                            price={item.precio}
-                            power={item.potencia}
-                            imagen={item.imagen}
-                            type={item.tipo}
-                            unitpower={item.unidadpotencia}
-                            profit={item.ganancia}
-                            consumption={item.consumo}
+//     }, []);
 
 
-                        />)
-                    )
-                }
-            </div>
+
+//     return (
+//         <div>
+//             <div class='barraseparar'></div>
+//             <div id='titulos'>
+//                 <h2>Mineria con GPU</h2>
+//             </div>
+//             <div class='barraseparar'></div>
+//             <section className="holder"> </section>
+//             <div class='barraseparar'></div>
+
+//             <div className="center">
+//                 {
+//                     loading ? (
+//                         <p>cargando...</p>
+
+//                     ) : (
+
+//                         mineria.map(item => <AsicsItem key={item.id}
+//                             name={item.nombre}
+//                             price={item.precio}
+//                             power={item.potencia}
+//                             imagen={item.imagen}
+//                             type={item.tipo}
+//                             unitpower={item.unidadpotencia}
+//                             profit={item.ganancia}
+//                             consumption={item.consumo}
+
+
+//                         />)
+//                     )
+//                 }
+//             </div>
            
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
 export default MineriaPage;
